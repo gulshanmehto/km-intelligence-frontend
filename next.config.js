afb4bbaa60c51765ff7eea0e27bdeb745358441a
gulshanmehto <gulshanmehto@gmail.com>
2025-08-12 15:22:20 +0530

    Update next.config.js with API rewrites

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://km-intelligence-backend.vercel.app/api/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
